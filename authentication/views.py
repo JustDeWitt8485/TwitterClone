4f3ad86f942d5132ea97ecfdb745b6bc76643246
Tracy DeWitt <Justdewitt8485@gmail.com>
2020-12-06 20:46:47 -0600

    Still working on functionality

---
+++ authentication/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
+
+from django.shortcuts import HttpResponseRedirect, render, reverse
+
+from twitteruser.models import TwitterUser
+
+from .forms import LoginForm
+
+# Create your views here.
+
+
+def index(request):
+    html = 'index.html'
+    return render(request, html,  {})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get('next',
+                                                            reverse('homepage')))
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(request.GET.get('next', reverse('loginpage')))
---
+++ twitteruser/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from twitteruser.models import TwitterUser
+
+# Register your models here.
+
+
+admin.site.register(TwitterUser, UserAdmin)
